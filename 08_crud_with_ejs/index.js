510754715826d3a464d0dd8a27a807f7f5dc1ca0
PRIYANSHU_PANT <pantpriyanshu707@gmail.com>
2025-10-21 16:42:27 +0000

    edit features added

--- 08_crud_with_ejs/index.js
+++ 08_crud_with_ejs/index.js
@@ -41,6 +41,18 @@ res.redirect("/read")
 })
 
 
+app.get('/edit/:userid',async function(req,res){
+  let user=await userModel.findOne({_id:req.params.userid});
+  res.render("edit",{user})
+})
+
+app.post('/update/:userid',async function(req,res){
+   let {name,email,image}=req.body;
+   let user=await userModel.findOneAndUpdate({_id:req.params.userid},{name,email,image},{new:true})
+   res.redirect("/read");
+   
+})
+
 app.listen(3000,function(){
  
     console.log("server is running fine ...")
